bf908a6764d5b6d72ea4b6287030818b423d5c6e
MexhitBici <146971656+MexhitBici@users.noreply.github.com>
2023-10-05 11:23:52 +0200

    1 file modified

--- src/app/trolley/trolley.component.ts
+++ src/app/trolley/trolley.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
-import { SumItemService } from '../sum-item.service';
+import { SumItemService } from '../add-item.service';
 
 @Component({
   selector: 'app-trolley',
